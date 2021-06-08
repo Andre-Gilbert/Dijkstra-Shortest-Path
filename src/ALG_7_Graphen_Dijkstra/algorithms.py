2c265fb96966497704bc9b4d44980e6fa36b785d
Valentin Müller <valentinmoritzmueller@gmail.com>
2021-06-08 15:41:29 +0200

    restructured dijkstra-package

--- src/ALG_7_Graphen_Dijkstra/algorithms.py
+++ src/ALG_7_Graphen_Dijkstra/algorithms.py
@@ -1,8 +1,9 @@
 """A dijkstra shortest path lazy implementation."""
 
+import heapq
 from queue import PriorityQueue
 
-from dijkstra_classes import Edge, Graph, Vertex
+from data_structures import Graph, Vertex
 
 
 def dijkstra_lazy(graph: Graph, start: Vertex, destination: Vertex) -> None:
@@ -44,26 +45,39 @@ def dijkstra_lazy(graph: Graph, start: Vertex, destination: Vertex) -> None:
     print(f'Distance: {costs[destination]}')
 
 
-if __name__ == "__main__":
-    v_a = Vertex('A')
-    v_b = Vertex('B')
-    v_c = Vertex('C')
-    v_d = Vertex('D')
-    v_e = Vertex('E')
-    v_f = Vertex('F')
-    vertices = {v_a, v_b, v_c, v_d, v_e, v_f}
+def dijkstra_eager(graph: Graph, start: Vertex, destination: Vertex) -> None:
+    heap = [(0, start)]
+    visited = {start}
+    came_from = {}
 
-    e_a_b = Edge(v_a, v_b, 7)
-    e_a_c = Edge(v_a, v_c, 9)
-    e_a_f = Edge(v_a, v_f, 14)
-    e_b_c = Edge(v_b, v_c, 10)
-    e_b_d = Edge(v_b, v_d, 15)
-    e_c_d = Edge(v_c, v_d, 11)
-    e_c_f = Edge(v_c, v_f, 2)
-    e_d_e = Edge(v_d, v_e, 6)
-    e_f_e = Edge(v_f, v_e, 9)
-    edges = {e_a_b, e_a_c, e_a_f, e_b_c, e_b_d, e_c_d, e_c_f, e_d_e, e_f_e}
+    costs = dict.fromkeys(graph.vertices, float('inf'))
 
-    graph = Graph(vertices, edges)
+    costs[start] = 0
 
-    dijkstra_lazy(graph, v_a, v_e)
+    while heap:
+        current = heapq.heappop(heap)[1]
+
+        for edge in current.adjacent_edges:
+            temp_distance = costs[current] + edge.cost
+
+            if temp_distance < costs[edge.destination]:
+                came_from[edge.destination] = current
+                costs[edge.destination] = temp_distance
+
+                if edge.destination not in visited:
+                    heapq.heappush(heap, (costs[edge.destination], edge.destination))
+                    visited.add(edge.destination)
+
+    current = destination
+    path = current.name
+
+    while current in came_from:
+        current = came_from[current]
+        path = f'{current.name} -> {path}'
+
+    print(f'Shortest Path: {path}')
+    print(f'Distance: {costs[destination]}')
+
+
+def a_star_search() -> None:
+    pass
---
+++ src/ALG_7_Graphen_Dijkstra/main.py
@@ -0,0 +1,44 @@
+import time
+
+from algorithms import a_star_search, dijkstra_eager, dijkstra_lazy
+from data_structures import Edge, Graph, Vertex
+
+if __name__ == "__main__":
+    v_a = Vertex('A')
+    v_b = Vertex('B')
+    v_c = Vertex('C')
+    v_d = Vertex('D')
+    v_e = Vertex('E')
+    v_f = Vertex('F')
+    vertices = {v_a, v_b, v_c, v_d, v_e, v_f}
+
+    e_a_b = Edge(v_a, v_b, 7)
+    e_a_c = Edge(v_a, v_c, 9)
+    e_a_f = Edge(v_a, v_f, 14)
+    e_b_c = Edge(v_b, v_c, 10)
+    e_b_d = Edge(v_b, v_d, 15)
+    e_c_d = Edge(v_c, v_d, 11)
+    e_c_f = Edge(v_c, v_f, 2)
+    e_d_e = Edge(v_d, v_e, 6)
+    e_f_e = Edge(v_f, v_e, 9)
+    edges = {e_a_b, e_a_c, e_a_f, e_b_c, e_b_d, e_c_d, e_c_f, e_d_e, e_f_e}
+
+    graph = Graph(vertices, edges)
+
+    start = time.time()
+    dijkstra_lazy(graph, v_a, v_e)
+    end = time.time()
+
+    print(f'Lazy Dijkstra took: {end - start} s')
+
+    start = time.time()
+    dijkstra_eager(graph, v_a, v_e)
+    end = time.time()
+
+    print(f'Eager Dijkstra took: {end - start} s')
+
+    start = time.time()
+    a_star_search(graph, v_a, v_e)
+    end = time.time()
+
+    print(f'A*-Search took {end - start} s')
