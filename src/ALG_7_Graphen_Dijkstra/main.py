d73e512394662798d7c28923c7d43ad7248edd79
André Gilbert <andre-anan.gilbert@sap.com>
2021-06-27 16:53:55 +0200

    updated docstrings, fixed typo

--- src/ALG_7_Graphen_Dijkstra/main.py
+++ src/ALG_7_Graphen_Dijkstra/main.py
@@ -7,6 +7,7 @@ Typical usage example:
     v_d = Vertex('D')
     v_e = Vertex('E')
     v_f = Vertex('F')
+    vertices = {v_a, v_b, v_c, v_d, v_e, v_f}
 
     e_a_b = Edge(v_a, v_b, 7)
     e_a_c = Edge(v_a, v_c, 9)
@@ -17,12 +18,20 @@ Typical usage example:
     e_c_f = Edge(v_c, v_f, 2)
     e_d_e = Edge(v_d, v_e, 6)
     e_f_e = Edge(v_f, v_e, 9)
-
-    vertices = {v_a, v_b, v_c, v_d, v_e, v_f}
     edges = {e_a_b, e_a_c, e_a_f, e_b_c, e_b_d, e_c_d, e_c_f, e_d_e, e_f_e}
+
     graph = Graph(vertices, edges)
 
+    print("\nDijkstra Lazy Version:")
+    print("-" * 31)
     dijkstra_lazy(graph, v_a, v_e)
+
+    # Reset visited vertices
+    for vertex in graph.vertices:
+        vertex.visited = False
+
+    print("\nDijkstra Eager Version:")
+    print("-" * 31)
     dijkstra_eager(graph, v_a, v_e)
 """
 from algorithms import dijkstra_eager, dijkstra_lazy
