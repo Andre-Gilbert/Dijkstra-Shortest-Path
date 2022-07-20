3787de00e4742416ba6152583db2f024bf647b1d
André-Anan Gilbert <andre.gilbert.77110@gmail.com>
2022-07-20 18:47:30 +0200

    Update utils.py

--- src/pathfinding_visualizer/utils.py
+++ src/pathfinding_visualizer/utils.py
@@ -52,10 +52,10 @@ class Path:
         current_src = intersection
         current_dst = intersection
         while current_src in came_from_src or current_dst in came_from_dst:
-            if current_src:
+            if current_src in came_from_src:
                 current_src = came_from_src[current_src]
                 current_src.make_path()
-            if current_dst:
+            if current_dst in came_from_dst:
                 current_dst = came_from_dst[current_dst]
                 current_dst.make_path()
                 
